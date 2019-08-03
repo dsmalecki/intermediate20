7cab270c706edbb97a4eb0af371a818f2dc73892
kf <sdakftraining@gmail.com>
2019-08-03 13:04:21 +0200

    customers begin

--- src/main/java/pl/sda/intermediate/Customer.java
+++ src/main/java/pl/sda/intermediate/Customer.java
@@ -9,16 +9,27 @@ public class Customer {
     private int age;
     private BigDecimal salary;
 
+    static int counter = 0;
+
+    { //blok inicjalizujący - uruchomi się ZAWSZE przy towrzeniu nowego obiektu
+        System.out.println("Blok inicjalizujący");
+        id = ++counter;
+    }
 
     public Customer(String firstName, String lastName, int age, String salary) {
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.salary = new BigDecimal(salary);
+        this(firstName, lastName, age, Integer.parseInt(salary));
+        System.out.println("Konstruktor 1");
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.age = age;
+//        this.salary = BigDecimal.valueOf(Double.valueOf(salary));
     }
 
     public Customer(String firstName, String lastName, int age, int salary) {
-
+        System.out.println("Konstruktor 2");
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = BigDecimal.valueOf(salary);
     }
 }
